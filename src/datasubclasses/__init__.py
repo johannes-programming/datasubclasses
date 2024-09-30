4bf29d1fc99d7bfd507eded116aec09b0ec21e09
Johannes <johannes-programming@mailfence.com>
2024-09-30 13:33:31 +0200

    Version 0.0.1.dev0

--- src/datasubclasses/__init__.py
+++ src/datasubclasses/__init__.py
@@ -1,6 +1,27 @@
-def main(args=None):
-    print("Hello World!")
+import functools
+from dataclasses import dataclass
+from typing import *
+
+__all__ = ["decorator", "init", "tool"]
 
 
-if __name__ == "__main__":
-    main()
+def decorator(cls: type, carry: Iterable, /, **kwargs) -> type:
+    cargo = dict()
+    for x in carry:
+        cargo[x] = getattr(cls, x)
+        delattr(cls, x)
+    cls = dataclass(cls, **kwargs)
+    cls = type(cls.__name__, (cls,), cargo)
+    return cls
+
+
+def init(**kwargs) -> type:
+    return tool("__init__", **kwargs)
+
+
+def tool(*args, **kwargs) -> type:
+    return functools.partial(
+        decorator,
+        args,
+        **kwargs,
+    )
--- src/datasubclasses/__main__.py
+++
@@ -1,4 +0,0 @@
-from datasubclasses import main
-
-if __name__ == "__main__":
-    main()
